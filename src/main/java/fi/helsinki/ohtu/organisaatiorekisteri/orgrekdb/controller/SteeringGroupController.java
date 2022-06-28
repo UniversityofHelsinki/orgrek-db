0c008946205f51bca2c874b670f7a5997f9ef5ce
Riku Hänninen <riku.hanninen@helsinki.fi>
2022-06-28 11:24:02 +0300

    OR-748 -- näytä kaikki iam-johtoryhmät

--- src/main/java/fi/helsinki/ohtu/organisaatiorekisteri/orgrekdb/controller/SteeringGroupController.java
+++ src/main/java/fi/helsinki/ohtu/organisaatiorekisteri/orgrekdb/controller/SteeringGroupController.java
@@ -30,13 +30,18 @@ public class SteeringGroupController {
     @RequestMapping(method = GET, value="/steeringGroups")
     public List<DegreeProgrammeDTO> getSteeringGroups() {
         List<DegreeProgrammeDTO> dtos = orgUnitDao.getDegreeProgrammesAndAttributes();
-        Map<String, SteeringGroup> steeringGroups = orgUnitDao.getSteeringGroups();
+        Map<String, List<SteeringGroup>> steeringGroups = orgUnitDao.getSteeringGroups();
         dtos.forEach(dto -> {
             String id = dto.getNodeId();
-            if ((steeringGroups.get(id) != null)) {
-                dto.setSteeringGroupNameEn(steeringGroups.get(id).getEn());
-                dto.setSteeringGroupNameFi(steeringGroups.get(id).getFi());
-                dto.setSteeringGroupNameSv(steeringGroups.get(id).getSv());
+            if ((steeringGroups.containsKey(id))) {
+                List<SteeringGroup> nodeSteeringGroups = steeringGroups.get(id);
+                nodeSteeringGroups.forEach(nsg -> {
+                    if (nsg.getIamGroup().equals(dto.getIamGroup())) {
+                        dto.setSteeringGroupNameEn(nsg.getEn());
+                        dto.setSteeringGroupNameFi(nsg.getFi());
+                        dto.setSteeringGroupNameSv(nsg.getSv());
+                    }
+                });
             }
         });
         return dtos;
