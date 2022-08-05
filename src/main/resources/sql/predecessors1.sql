51766f592c8fc81ff58fb9bf99babdc5b7559359
Riku Hänninen <riku.hanninen@helsinki.fi>
2022-08-05 13:20:39 +0300

    Useampi nimi niin otetaan vain uusin

--- src/main/resources/sql/predecessors1.sql
+++ src/main/resources/sql/predecessors1.sql
@@ -1,4 +1,4 @@
-SELECT PN.ID, PN.UNIQUE_ID, PN.START_DATE, PN.END_DATE, P.START_DATE edge_start_date, P.END_DATE edge_end_date, FN.NAME full_name, FN.LANGUAGE FROM PREDECESSOR_RELATION P
+SELECT PN.ID, PN.UNIQUE_ID, PN.START_DATE, PN.END_DATE, P.START_DATE edge_start_date, P.END_DATE edge_end_date, FN.LANGUAGE, max(fn.name) keep (dense_rank first order by fn.start_date desc nulls last, fn.end_date desc nulls first) full_name FROM PREDECESSOR_RELATION P
     JOIN NODE N ON P.NODE_ID = N.ID
     JOIN NODE PN ON P.PREDECESSOR_ID = PN.ID
     JOIN FULL_NAME FN on P.PREDECESSOR_ID = FN.NODE_ID
@@ -9,4 +9,4 @@ SELECT PN.ID, PN.UNIQUE_ID, PN.START_DATE, PN.END_DATE, P.START_DATE edge_start_
         OR (PN.END_DATE < trunc(to_date(:date, 'DD.MM.YYYY')) AND
             (FN.START_DATE IS NULL OR FN.START_DATE <= PN.END_DATE)
             AND (FN.END_DATE IS NULL OR FN.END_DATE >= PN.END_DATE)))
-where P.NODE_ID = :nodeId
+where P.NODE_ID = :nodeId group by PN.ID, PN.UNIQUE_ID, PN.START_DATE, PN.END_DATE, P.START_DATE, P.END_DATE, FN.LANGUAGE
