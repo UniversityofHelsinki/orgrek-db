8654d83402b33858d6dc15f7ca8e5e24c995e1b9
Riku Hänninen <riku.hanninen@helsinki.fi>
2022-08-24 15:23:40 +0300

    OR-792 - hierarchy_filter-taulukkoon tarvittavat backend-muutokset

--- src/main/java/fi/helsinki/ohtu/organisaatiorekisteri/orgrekdb/dao/HierarchyFilterDao.java
+++ src/main/java/fi/helsinki/ohtu/organisaatiorekisteri/orgrekdb/dao/HierarchyFilterDao.java
@@ -1,16 +1,21 @@
 package fi.helsinki.ohtu.organisaatiorekisteri.orgrekdb.dao;
 
 import fi.helsinki.ohtu.organisaatiorekisteri.orgrekdb.domain.HierarchyFilter;
+import fi.helsinki.ohtu.organisaatiorekisteri.orgrekdb.util.ReadSqlFiles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository(value = "hierarchyFilterDao")
 public class HierarchyFilterDao extends NamedParameterJdbcDaoSupport {
@@ -52,6 +57,50 @@ public class HierarchyFilterDao extends NamedParameterJdbcDaoSupport {
         params.addValue("attrstart", attrstart);
         List<HierarchyFilter> hierarchyFilters = getNamedParameterJdbcTemplate().query(sql, params, BeanPropertyRowMapper.newInstance(HierarchyFilter.class));
         return hierarchyFilters;
+    }
+
+    public List<HierarchyFilter> getHierarchyFilters() throws IOException  {
+        String sql = ReadSqlFiles.sqlString("hierarchyFilters.sql");
+
+        List<HierarchyFilter> hierarchyFilters = getNamedParameterJdbcTemplate().query(sql, BeanPropertyRowMapper.newInstance(HierarchyFilter.class));
+        return hierarchyFilters;
+    }
+
+    public int updateHierarchyFilter(HierarchyFilter hierarchyFilter) throws IOException {
+        String sql = ReadSqlFiles.sqlString("updateHierarchyFilter.sql");
+
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("id", hierarchyFilter.getId());
+        params.addValue("hierarchy", hierarchyFilter.getHierarchy());
+        params.addValue("key", hierarchyFilter.getKey());
+        params.addValue("value", hierarchyFilter.getValue());
+        params.addValue("startDate", hierarchyFilter.getStartDate());
+        params.addValue("endDate", hierarchyFilter.getEndDate());
+        return getNamedParameterJdbcTemplate().update(sql, params);
+    }
+
+    public int deleteHierarchyFilter(HierarchyFilter hierarchyFilter) throws IOException {
+        String sql = ReadSqlFiles.sqlString("deleteHierarchyFilter.sql");
+
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("id", hierarchyFilter.getId());
+        return getNamedParameterJdbcTemplate().update(sql, params);
+    }
+
+    public int[] insertHierarchyFilters(List<HierarchyFilter> hierarchyFilters) throws IOException {
+        String sql = ReadSqlFiles.sqlString("insertHierarchyFilters.sql");
+
+        MapSqlParameterSource[] paramMaps = hierarchyFilters.stream().map(hierarchyFilter -> {
+            MapSqlParameterSource params = new MapSqlParameterSource();
+            params.addValue("hierarchy", hierarchyFilter.getHierarchy());
+            params.addValue("key", hierarchyFilter.getKey());
+            params.addValue("value", hierarchyFilter.getValue());
+            params.addValue("startDate", hierarchyFilter.getStartDate());
+            params.addValue("endDate", hierarchyFilter.getEndDate());
+            return params;
+        }).collect(Collectors.toList()).toArray(new MapSqlParameterSource[]{});
+        int[] result = getNamedParameterJdbcTemplate().batchUpdate(sql, paramMaps);
+        return result;
     }
 }
 
