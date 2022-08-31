6715aecf3c3efaf64a709832f09846abec41f09a
vmheikki <veli-matti.heikkila@helsinki.fi>
2022-08-31 10:15:03 +0300

    add node_type start date and end date checks to sql

--- src/main/resources/sql/degreeProgramme.sql
+++ src/main/resources/sql/degreeProgramme.sql
@@ -1,10 +1,12 @@
 SELECT NODE.* FROM NODE JOIN NODE_TYPE ON NODE.ID = NODE_TYPE.NODE_ID
-                WHERE NODE.ID IN
-                (SELECT distinct CHILD_NODE_ID
-                FROM (SELECT * FROM edge WHERE
-                   (END_DATE IS NULL OR END_DATE > trunc(:today))
-                   AND (START_DATE IS NULL OR START_DATE <= trunc(:today))
-                   AND hierarchy = 'toiminnanohjaus')
-                START WITH PARENT_NODE_ID = :nodeId
-                CONNECT BY PRIOR CHILD_NODE_ID = PARENT_NODE_ID)
-                AND NODE_TYPE.VALUE IN ('kandiohjelma', 'maisteriohjelma', 'tohtoriohjelma')
+WHERE NODE.ID IN
+      (SELECT distinct CHILD_NODE_ID
+       FROM (SELECT * FROM edge WHERE
+           (END_DATE IS NULL OR END_DATE > trunc(:today))
+                                  AND (START_DATE IS NULL OR START_DATE <= trunc(:today))
+                                  AND hierarchy = 'toiminnanohjaus')
+       START WITH PARENT_NODE_ID = :nodeId
+       CONNECT BY PRIOR CHILD_NODE_ID = PARENT_NODE_ID)
+    AND NODE_TYPE.VALUE IN ('kandiohjelma', 'maisteriohjelma', 'tohtoriohjelma')
+    AND NODE_TYPE.END_DATE IS NULL OR NODE_TYPE.END_DATE > trunc(:today)
+    AND (NODE_TYPE.START_DATE IS NULL OR NODE_TYPE.START_DATE <= trunc(:today))
