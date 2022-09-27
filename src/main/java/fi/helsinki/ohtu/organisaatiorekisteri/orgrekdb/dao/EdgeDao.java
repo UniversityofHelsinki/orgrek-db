e28588feaf3d234bf5ce35f8faa4fe428a9e78fe
vmheikki <veli-matti.heikkila@helsinki.fi>
2022-09-27 13:03:24 +0300

    batch update method to update parent unit dates

--- src/main/java/fi/helsinki/ohtu/organisaatiorekisteri/orgrekdb/dao/EdgeDao.java
+++ src/main/java/fi/helsinki/ohtu/organisaatiorekisteri/orgrekdb/dao/EdgeDao.java
@@ -1,15 +1,21 @@
 package fi.helsinki.ohtu.organisaatiorekisteri.orgrekdb.dao;
 
+import fi.helsinki.ohtu.organisaatiorekisteri.orgrekdb.domain.EdgeWrapper;
 import fi.helsinki.ohtu.organisaatiorekisteri.orgrekdb.util.ReadSqlFiles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository(value = "edgeDao")
 public class EdgeDao extends NamedParameterJdbcDaoSupport {
@@ -28,6 +34,21 @@ public class EdgeDao extends NamedParameterJdbcDaoSupport {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
         List<String> hierarchies = jdbcTemplate.queryForList(sql, String.class);
         return hierarchies;
+    }
+
+    public int[] updateParentUnitProperties(List<EdgeWrapper> parentUnitProperties) throws IOException {
+        String sql = ReadSqlFiles.sqlString("updateParentUnitProperties.sql");
+
+        MapSqlParameterSource[] paramMaps = parentUnitProperties.stream().map(property -> {
+            MapSqlParameterSource params = new MapSqlParameterSource();
+            params.addValue("startDate", property.getStartDate());
+            params.addValue("endDate", property.getEndDate());
+            params.addValue("parentNodeId", property.getParentNodeId());
+            params.addValue("childNodeId", property.getChildNodeId());
+            params.addValue("hierarchy", property.getHierarchy());
+            return params;
+        }).collect(Collectors.toList()).toArray(new MapSqlParameterSource[]{});
+        return getNamedParameterJdbcTemplate().batchUpdate(sql, paramMaps);
     }
 
 }
