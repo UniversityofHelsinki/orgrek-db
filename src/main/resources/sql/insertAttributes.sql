63997b2500e3aa9456e900bcbb68018c742b706e
vmheikki <veli-matti.heikkila@helsinki.fi>
2023-03-06 10:10:29 +0200

    changed insertAttributes.sql nodeId to node_id

--- src/main/resources/sql/insertAttributes.sql
+++ src/main/resources/sql/insertAttributes.sql
@@ -1,2 +1,2 @@
 INSERT INTO NODE_ATTR (ID, NODE_ID, KEY, VALUE, START_DATE, END_DATE)
-VALUES (:id, :nodeId, :key, :value, TO_DATE(:start_date, 'DD.MM.YYYY'), TO_DATE(:end_date, 'DD.MM.YYYY'))
+VALUES (:id, :node_id, :key, :value, TO_DATE(:start_date, 'DD.MM.YYYY'), TO_DATE(:end_date, 'DD.MM.YYYY'))
