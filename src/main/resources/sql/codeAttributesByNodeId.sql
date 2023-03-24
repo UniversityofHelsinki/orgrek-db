e2c65bd068403944627ef299128c65cf5f4f977b
Emmi Sulander <emmi.sulander@helsinki.fi>
2023-03-24 13:16:16 +0200

    Update list of code attributes in sql

--- src/main/resources/sql/codeAttributesByNodeId.sql
+++ src/main/resources/sql/codeAttributesByNodeId.sql
@@ -1,1 +1,10 @@
-SELECT * FROM NODE_ATTR WHERE NODE_ID = :node_id AND (KEY = 'laskutus_tunnus' OR KEY = 'mainari_tunnus' OR KEY = 'talous_tunnus' OR KEY = 'oppiaine_tunnus' OR KEY = 'hr_lyhenne' OR KEY = 'lyhenne' OR KEY = 'emo_lyhenne')
+SELECT * FROM NODE_ATTR WHERE NODE_ID = :node_id AND (KEY = 'lyhenne' OR
+                                                      KEY = 'emo_lyhenne' OR
+                                                      KEY = 'hr_lyhenne' OR
+                                                      KEY = 'iam_ryhma' OR
+                                                      KEY = 'hr_tunnus' OR
+                                                      KEY = 'laskutus_tunnus' OR
+                                                      KEY = 'mainari_tunnus' OR
+                                                      KEY = 'oppiaine_tunnus' OR
+                                                      KEY = 'talous_tunnus' OR
+                                                      KEY = 'tutkimus_tunnus')
