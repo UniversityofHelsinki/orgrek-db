427dbc1917950a35ad962cc1a0ceda9b7b669f63
Antti Lankinen <antti.lankinen@helsinki.fi>
2023-04-06 06:20:59 +0300

    start_date ja end_date päivitys ja lisäys korjattu: FROM_TZ(TO_TIMESTAMP ... attribuutin päivityksessä ja lisäyksessä.

--- src/main/resources/sql/insertAttributes.sql
+++ src/main/resources/sql/insertAttributes.sql
@@ -1,2 +1,2 @@
 INSERT INTO NODE_ATTR (ID, NODE_ID, KEY, VALUE, START_DATE, END_DATE)
-VALUES (:id, :node_id, :key, :value, TO_DATE(:start_date, 'DD.MM.YYYY'), TO_DATE(:end_date, 'DD.MM.YYYY'))
+VALUES (:id, :node_id, :key, :value, FROM_TZ(TO_TIMESTAMP(:start_date, 'DD.MM.YYYY'),'0:00'), FROM_TZ(TO_TIMESTAMP(:end_date, 'DD.MM.YYYY'),'0:00'))
--- src/main/resources/sql/updateAttributes.sql
+++ src/main/resources/sql/updateAttributes.sql
@@ -1,3 +1,3 @@
 UPDATE NODE_ATTR
-SET NODE_ID = :node_id, KEY = :key, VALUE= :value, START_DATE = TO_DATE(:start_date, 'DD.MM.YYYY'), END_DATE = TO_DATE(:end_date, 'DD.MM.YYYY')
+SET NODE_ID = :node_id, KEY = :key, VALUE= :value, START_DATE = FROM_TZ(TO_TIMESTAMP(:start_date, 'DD.MM.YYYY'),'0:00'), END_DATE = FROM_TZ(TO_TIMESTAMP(:end_date, 'DD.MM.YYYY'),'0:00')
 WHERE ID = :id AND NODE_ID = :node_id
