6dfb8a5b344918eacf719c44af47cf9bef628990
Antti Lankinen <antti.lankinen@helsinki.fi>
2023-04-20 13:51:47 +0300

    Pahasti kesken.

--- src/main/java/fi/helsinki/ohtu/organisaatiorekisteri/orgrekdb/controller/EdgeController.java
+++ src/main/java/fi/helsinki/ohtu/organisaatiorekisteri/orgrekdb/controller/EdgeController.java
@@ -1,12 +1,19 @@
 package fi.helsinki.ohtu.organisaatiorekisteri.orgrekdb.controller;
 
 import fi.helsinki.ohtu.organisaatiorekisteri.orgrekdb.dao.EdgeDao;
+import fi.helsinki.ohtu.organisaatiorekisteri.orgrekdb.domain.EdgeWrapper;
+import fi.helsinki.ohtu.organisaatiorekisteri.orgrekdb.service.EdgePropertiesService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/edge")
@@ -14,10 +21,22 @@ public class EdgeController {
 
     @Autowired
     private EdgeDao edgeDao;
+    @Autowired
+    private EdgePropertiesService edgePropertiesService;
 
     @RequestMapping("/types")
     public List<String> getHierarchyTypes() throws IOException {
         return edgeDao.getHierarchyTypes();
+    }
+
+    @PutMapping("/parent/units")
+    public ResponseEntity<Map<String, List<EdgeWrapper>>> updateParents(@RequestBody Map<String, List<EdgeWrapper>> attributesMap) {
+        try {
+            edgePropertiesService.updateDeleteOrSaveUpperUnit(attributesMap);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
 
