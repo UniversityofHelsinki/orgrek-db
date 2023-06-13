d3563e61e4f4e9b3df25674017df081511100f07
vmheikki <veli-matti.heikkila@helsinki.fi>
2023-06-13 14:05:03 +0300

    removed updateAttributes put and post methods

--- src/main/java/fi/helsinki/ohtu/organisaatiorekisteri/orgrekdb/controller/NodeAttributeController.java
+++ src/main/java/fi/helsinki/ohtu/organisaatiorekisteri/orgrekdb/controller/NodeAttributeController.java
@@ -53,60 +53,6 @@ public class NodeAttributeController {
         return orgUnitDao.getFutureAndCurrentAttributeListByDate(node.getId(), dateObj);
     }
 
-    @PutMapping("/attributes/{nodeId}/{skipValidation}")
-    public ResponseEntity<List<Attribute>> updateAttributes(@PathVariable("nodeId") String nodeId,
-                                                            @PathVariable("skipValidation") boolean skipValidation,
-                                                            @RequestBody List<Attribute> attributes) throws IOException {
-        List<Attribute> conflictingAttributes = new ArrayList<>();
-        if(!skipValidation) {
-            conflictingAttributes = validate(attributes, skipValidation);
-        }
-        if(skipValidation || conflictingAttributes.isEmpty()) {
-            attributeDao.updateAttributes(attributes);
-            return new ResponseEntity<>(attributes, HttpStatus.OK);
-        }
-        if(!conflictingAttributes.isEmpty()) {
-            return new ResponseEntity<>(conflictingAttributes, HttpStatus.CONFLICT);
-        }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    }
-
-    private List<Attribute> validate(List<Attribute> attributes, boolean skipValidation) throws IOException {
-        List<Attribute> ret = new ArrayList<>();
-        for(Attribute attribute : attributes) {
-            if(!skipValidation || !isFullnameAttribute(attribute)) {
-                Attribute attr = attributeDao.checkIfExists(attribute);
-                if (attr != null) {
-                    ret.add(attr);
-                }
-            }
-        }
-        return ret;
-    }
-
-    @PostMapping("/attributes/{nodeId}/{skipValidation}")
-    public ResponseEntity<Attribute> insertAttribute(@PathVariable("nodeId") String nodeId,
-                                                     @PathVariable("skipValidation") boolean skipValidation,
-                                                     @RequestBody Attribute attribute) throws IOException {
-        Attribute existingAttribute = null;
-        if(!skipValidation || !isFullnameAttribute(attribute)) {
-            existingAttribute = attributeDao.getExistingAttribute(attribute);
-        }
-        if(skipValidation || existingAttribute==null) {
-            Attribute created = attributeDao.insertAttribute(attribute);
-            return new ResponseEntity<>(created, HttpStatus.CREATED);
-        }
-        else {
-            return new ResponseEntity<>(existingAttribute, HttpStatus.CONFLICT);
-        }
-    }
-
-    private boolean isFullnameAttribute(Attribute attribute) {
-        List fullnameAttributes = Arrays.asList(new String[]{"emo_lyhenne", "lyhenne", "name_fi", "name_en", "name_sv"});
-        return fullnameAttributes.contains(attribute.getKey());
-    }
-
-
     @PutMapping("/name/attributes")
     public ResponseEntity<Map<String, List<Attribute>>> updateNameAttributes(@RequestBody Map<String, List<Attribute>> attributesMap) {
         try {
