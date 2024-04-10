69965a1484b6d1209846e19b7ddb5caaf9618b68
Riku Hänninen <riku.hanninen@helsinki.fi>
2024-04-10 08:44:46 +0300

    OR-1289
    - financeUnitsPublic api returning all units in finance units having
      publicity attribute in node_attr-table with a value of 'kylla'

--- src/main/java/fi/helsinki/ohtu/organisaatiorekisteri/orgrekdb/dao/OrgUnitDao.java
+++ src/main/java/fi/helsinki/ohtu/organisaatiorekisteri/orgrekdb/dao/OrgUnitDao.java
@@ -20,6 +20,8 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Repository;
 
+import com.fasterxml.jackson.annotation.JsonView;
+
 import fi.helsinki.ohtu.organisaatiorekisteri.orgrekdb.domain.Attribute;
 import fi.helsinki.ohtu.organisaatiorekisteri.orgrekdb.domain.DegreeProgrammeDTO;
 import fi.helsinki.ohtu.organisaatiorekisteri.orgrekdb.domain.EdgeWrapper;
@@ -301,6 +303,14 @@ public class OrgUnitDao extends NamedParameterJdbcDaoSupport {
           sql, BeanPropertyRowMapper.newInstance(FinanceUnit.class)
       );
     };
+
+    public List<FinanceUnit> getPublicFinanceUnits() throws IOException {
+      String sql = ReadSqlFiles.sqlString("financeUnitsPublic.sql");
+      return getNamedParameterJdbcTemplate().query(
+          sql, BeanPropertyRowMapper.newInstance(FinanceUnit.class)
+      );
+    }
+
 
     public List<EducationUnit> getEducationUnits() throws IOException {
       String sql = ReadSqlFiles.sqlString("educationUnits.sql");
