788805ee7096ab10e1c4edf79303c6f137d54eac
anttilan <antti.lankinen@helsinki.fi>
2024-07-03 14:00:13 +0300

    Poista Full_name käytöstä

--- src/main/resources/sql/fullNamesByNodeIds.sql
+++ src/main/resources/sql/fullNamesByNodeIds.sql
@@ -1,3 +1,14 @@
-SELECT * FROM FULL_NAME WHERE NODE_ID IN (:nodeIds)
+SELECT
+    NODE_ID,
+    VALUE AS NAME,
+    CASE
+        WHEN KEY = 'name_fi' THEN 'fi'
+        WHEN KEY = 'name_en' THEN 'en'
+        WHEN KEY = 'name_sv' THEN 'sv'
+        ELSE NULL END
+    AS LANGUAGE,
+    START_DATE,
+    END_DATE
+FROM NODE_ATTR WHERE NODE_ID IN (:nodeIds) AND KEY IN ('name_fi', 'name_sv', 'name_en')
   AND (START_DATE IS NULL OR START_DATE <= TRUNC(TO_DATE(:date, 'DD.MM.YYYY')))
   AND (END_DATE IS NULL OR END_DATE >= TRUNC(TO_DATE(:date, 'DD.MM.YYYY')))
